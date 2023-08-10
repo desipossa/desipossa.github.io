7de4baa5e86bc2fcc3a88105ba2c7346abf668f3
desipossa <c1jang@hanmail.net>
2023-08-10 09:49:53 +0900

    20230810

--- js/custom.js
+++ js/custom.js
@@ -9,7 +9,35 @@ const H = gsap.to(panels, {
         pin: true,
         start: "top top",
         scrub: 2, // 숫자에따라 변함이 있음
-        markers: true
+        markers: true,
+        snap: {
+            snapTo: 1 / (panels.length - 1),
+            inertia: false,
+            duration: { min: 0.1, max: 0.1 }
+        },
+        //end: () => "+=" + (panelsContainer.offsetWidth - innerWidth)
+    }
+});
+
+
+// const T = document.querySelector('#to_top');
+// T.addEventListener('click', () => {
+//     gsap.to(window, { scrollTo: 0, duration: 1 })
+// });
+
+
+
+const D = gsap.to('.MainDal .dal', {
+    x: 400,
+    y: 600,
+    rotation: 720,
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".MainDal",
+        pin: true,
+        start: "top top",
+        scrub: 0, // 숫자에따라 변함이 있음
+        //markers: true
         // snap: {
         //     snapTo: 1 / (panels.length - 1),
         //     inertia: false,
@@ -17,10 +45,5 @@ const H = gsap.to(panels, {
         // },
         //end: () => "+=" + (panelsContainer.offsetWidth - innerWidth)
     }
-});
 
-
-const T = document.querySelector('#to_top');
-T.addEventListener('click', () => {
-    gsap.to(window, { scrollTo: 0, duration: 1 })
 })
